17810f2d82340a1746877a9b06439d7e3172fab3
TheFamousChineeMagician <TheFamousChineeMagician@users.noreply.github.com>
2017-06-26 14:56:07 -1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function init(){
-	
+	 game.renderer.renderSession.roundPixels = true;
 }
 
 function preload(){
@@ -9,27 +9,76 @@ function preload(){
     game.load.image('grass:8x1', 'images/grass_8x1.png');
     game.load.image('grass:6x1', 'images/grass_6x1.png');
     game.load.image('grass:1x1', 'images/grass_1x1.png');
+    game.load.image('grass:4x1', 'images/grass_4x1.png');
+    game.load.image('grass:2x1', 'images/grass_2x1.png');
+    game.load.image('hero', 'images/hero_stopped.png');
 
-     game.load.image('hero', 'images/hero_stopped.png');
 };
 
 function create(){
 	game.add.image(0, 0, 'background');
 	loadLevel(this.game.cache.getJSON('level:1'));
+	leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+	rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
 }
+
 
 function update(){
-
-}
+	 handleInput();
+	 handleCollisions();
+};
 
 function loadLevel(data) {
 	console.log(data);
+	platforms = game.add.group();
 	 data.platforms.forEach(spawnPlatform, this);
 	 spawnCharacters({hero: data.hero});
+	 game.physics.arcade.gravity.y = 1200;
+
+	 
 };
 
 function spawnPlatform(platform) {
     game.add.sprite(platform.x, platform.y, platform.image);
+    var sprite = platforms.create(platform.x, platform.y, platform.image);
+    console.log(sprite);
+    game.physics.enable(sprite);
+    sprite.body.allowGravity = false;
+};
+
+function spawnCharacters(data){
+	hero = game.add.sprite(data.hero.x, data.hero.y, 'hero');
+	hero.anchor.set(0.5, 0.5);
+	game.physics.enable(hero);
+	hero.body.collideWorldBounds = true;
+	game.physics.enable(hero);
+	 hero.body.collideWorldBounds = true;
+};
+
+function move(direction){
+    hero.body.velocity.x = direction * 200;
+ 	if (hero.body.velocity.x < 0){
+        hero.scale.x = -1;
+    }
+    else if (hero.body.velocity.x > 0) {
+        hero.scale.x = 1;
+    }
+}
+
+function handleInput(){
+    if (leftKey.isDown) { 
+        move(-1);
+    }
+    else if (rightKey.isDown) { 
+    	move(1);
+    }
+     else {
+        move(0);
+	}
+}
+
+function handleCollisions(){
+   game.physics.arcade.collide(hero, platforms);
 };
 
 //Create a game state
